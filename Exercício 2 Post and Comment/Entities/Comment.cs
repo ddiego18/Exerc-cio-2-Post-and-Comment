5539499497a1777d5bffccee36ae8d77741d455a
ddiego18 <78805407+ddiego18@users.noreply.github.com>
2021-04-21 14:40:42 -0300

    Subir código do programa.

---
+++ Exercício 2 Post and Comment/Entities/Comment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercício_2_Post_and_Comment.Entities
+{
+    class Comment
+    {
+        public string Text { get; set; }
+
+        public Comment()
+        {
+
+        }
+
+        public Comment(string text)
+        {
+            Text = text;
+        }
+    }
+}
